18164cf471603c97becd0ed0ae9f4bcdb6895e35
Willdta <beyondutraining@gmail.com>
2018-04-22 16:02:39 -0400

    added check to see whether user has created a profile

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { getCurrentProfile } from '../actions/profileActions'
 
 class Dashboard extends Component { 
@@ -9,11 +10,31 @@ class Dashboard extends Component {
 
   render() {
     const { user } = this.props.auth
+    const { profile, loading } = this.props.profile
+
+    let dashboardContent
+
+    if (profile === null || loading === true) {
+      dashboardContent = <h4>Loading...</h4>
+    } else if (Object.keys(profile).length > 0) {
+      dashboardContent = (
+        <div>
+          <h1>Welcome { user.name }</h1>
+          <img src={ user.avatar } alt="" />
+        </div>
+      )
+    } else {
+      dashboardContent = (
+        <div>
+          <h5>You haven't created a profile yet</h5>
+          <Link to="/create-profile">Create profile</Link>
+        </div>
+      )
+    }
 
     return (
       <div>
-        <h1>Welcome { user.name }</h1>
-        <img src={ user.avatar } alt="" />
+        { dashboardContent }
       </div>
     )
   }
