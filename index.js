3cf9b70a7f6e5b293efa65abf390ae1d7dde6ad7
Willdta <beyondutraining@gmail.com>
2018-04-29 12:52:22 -0400

    port change

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ if (process.env.NODE_ENV === 'production') {
 // Port
 const port = process.env.PORT || 5000
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log('App listening on port 5000!');
 })
