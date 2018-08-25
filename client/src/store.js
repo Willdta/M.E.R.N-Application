f4185171ac2d25e03ccb55e6c99cc71d211ae74c
Willdta <beyondutraining@gmail.com>
2018-08-25 18:23:56 -0400

    deploy to heroku

--- client/src/store.js
+++ client/src/store.js
@@ -4,10 +4,10 @@ import {
   compose
 } from 'redux'
 import thunk from 'redux-thunk'
-import reducer from './reducers'
+import reducers from './reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(reducer, {}, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(thunk)))
 
 export default store
