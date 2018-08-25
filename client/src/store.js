1412949e037b68ee8ff5cf128a549f53020421bb
Willdta <beyondutraining@gmail.com>
2018-08-25 18:18:16 -0400

    rewrote redux dev tools compose code

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux'
 import thunk from 'redux-thunk'
-
 import reducer from './reducers'
 
-const middleware = [thunk]
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(reducer, {}, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(reducer, {}, composeEnhancers(applyMiddleware(thunk)))
 
 export default store
